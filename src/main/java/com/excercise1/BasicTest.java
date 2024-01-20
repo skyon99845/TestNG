0e86b9982d78a95ae525dd1b2d14b75446f420ee
skyon99845 <skyon9801@gmail.com>
2024-01-20 07:53:20 +0530

    first commit

---
+++ src/main/java/com/excercise1/BasicTest.java
@@ -0,0 +1,16 @@
+package com.excercise1;
+
+
+import org.testng.annotations.Test;
+
+public class BasicTest {
+    public static void main(String[] args) {
+        System.out.println("Welcome Shubham");
+    }
+
+    @Test
+    public void method1()
+    {
+        System.out.println("Great Day");
+    }
+}
